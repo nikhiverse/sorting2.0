e12dcd58141bdc00e88de48e88d7e96b4b8516e9
nikhilkr3 <nikhilkr3599@gmail.com>
2026-03-19 20:20:00 +0530

    fix minor bug

--- array.cpp
+++ array.cpp
@@ -22,7 +22,12 @@ static void generateRandomArray(int length) {
     }
 
     // Fisher-Yates shuffle
-    srand(time(NULL));
+    static bool seeded = false;
+    if (!seeded) {
+        srand(time(NULL));
+        seeded = true;
+    }
+
     for (int i = POOL_SIZE - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         int temp = pool[i];
